218ff378edc545cecd41d21c4a55cf35542a8534
Demir Dereli <52353085+demurthelemur@users.noreply.github.com>
2022-12-21 14:28:53 +0300

    Create Player.cs

---
+++ 27763_Akdeniz_EminBora_Step1/server_v2/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Player
+{
+    public string player_name { get; set; }
+    public Socket socket { get; set; }
+
+    public double score;
+    public Player(string player_name, Socket socket)
+    {
+        this.player_name = player_name;
+        this.socket = socket;
+        this.score = 0;
+    }
+}
+
